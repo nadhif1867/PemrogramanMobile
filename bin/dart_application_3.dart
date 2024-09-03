c92519f5a5d484d87e38f9fd6b98c59dac4776d9
nadhif1867 <0106.nadhif@gmail.com>
2024-09-03 13:00:40 +0700

    first commit

---
+++ bin/dart_application_3.dart
@@ -0,0 +1,16 @@
+import 'package:dart_application_3/dart_application_3.dart' as dart_application_3;
+// void main(List<String> arguments) {
+//   print('Hello world: ${dart_application_1.calculate()}!');
+// }
+
+// void main(List<String> arguments) {
+//   for (int i = 0; i < 10; i++){
+//     print('hello ${i + 2}');
+//   }
+// }
+
+void main(List<String> arguments) {
+  for (int i = 18; i > 8 ; i--){
+    print('Nama saya adalah Fulan, sekarang berumur ${i}');
+  }
+}
